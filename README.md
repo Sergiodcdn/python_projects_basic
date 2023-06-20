c80c78e4b507310f0008cb2740de25cc358fff73
Sergio Donato Camacho do Nascimento <sergiodcdn@gmail.com>
2023-06-20 13:17:41 -0300

    up

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # python_projects_basic
- Projetos Simples
+ Projetos Simples, uma releitura dos projetos de kying18.
+
+Você pode acessar a pasta original dela em :
+https://github.com/kying18/beginner-projects/blob/master/sample_madlibs/code.py
---
+++ amostras_madlibs/code.py
@@ -0,0 +1,26 @@
+def madlib():
+
+    parte_corpo = input("Parte do corpo: ")
+    verbo = input("Verbo: ")
+    adj1 = input("Adjectivo: ")
+    adj2 = input("Adjectivo: ")
+    adj3 = input("Adjectivo: ")
+    adj4 = input("Adjectivo: ")
+    adj5 = input("Adjectivo: ")
+    subst1 = input("Substantivo: ")
+    subst2 = input("Substantivo: ")
+    subst_plural_1 = input("Substantivo (plural): ")
+    subst_plural_2 = input("Substantivo (plural): ")
+
+
+    madlib = f"Adoro programação de computadores, porque é {adj1}! \
+    A jornada para se tornar um bom programador começa com esperança em sua mente, \
+    e um sonho em seu {parte_corpo}. Através do sangue, suor e {subst_plural_1}, \
+    espero que se torne eterno. Sim, uma vez que você aprender a {verbo}, irá se tornar \
+    parte de sua identidade, seu estilo de vida,  te tornarnando um super hacker {adj2}. \
+    Conhecimento de programação permite que você assuma o controle de seu {subst1}. \
+    Você pode criar seu próprio {subst_plural_2} pessoal, qualquer coisa \
+    desde desenvolver software {adj3} até analisar dados e fazer previsões sobre {subst2}. Você pode \
+    talvez até recriar um robô e torná-lo muito {adj4}. Espero que triunfe em sua jornada, seu {adj5}!"
+    
+    print(madlib)
